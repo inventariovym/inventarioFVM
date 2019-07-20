38ba9789dd9b258482dd7face871cfd7b6800713
sebastian <sebastian.tamayo@correounivalle.edu.co>
2019-07-20 00:54:53 -0500

    Pruebas para ver el llenado de las tablas

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const ruta = express.Router();
 const passport = require('passport');
+
 const db = require('../database');
+const config = require('pg').Pool;
+
+const pool = new config({ //Configuracion base de datos
+    user: 'postgres',
+    host: 'localhost',
+    database: 'inventario',
+    password: '1',
+    port: 5432
+})
 
 ///RUTAS
 ruta.get('/', (req, res) => {  //Muestra el login.html
     res.render('login.html');
 });
 
-ruta.get('/navegacion', (req, res) => {
-    res.render('inicio.html');
+ruta.get('/navegacion', async (req, res) => { //PARA PRUEBA DE COMO LLENAR UNA TABLA CON DATOS
+    await pool.query('SELECT * FROM login', function (err, resultado, fields) {
+        const datos = resultado.rows;
+        res.render('inicio.html', { datos });
+    });
 });
 
 ///PETICIONES
-ruta.post('/', db.postSignUp);
+ruta.post('/', db.postSignUp); //INICIO DE SESION FUNCIONANDO
+
 
 
 module.exports = ruta; 
