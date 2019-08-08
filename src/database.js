1544434a2c7ed09f44a31101e761d7a233ca7bf7
sebastian <sebastian.tamayo@correounivalle.edu.co>
2019-08-08 13:11:22 -0500

    Se añadio menu de navegacion y notficacion de error si ingresa mal el usuario o contraseña

--- src/database.js
+++ src/database.js
@@ -24,33 +24,35 @@ module.exports = {
           req.session.username = req.body.username;
           req.session.password = req.body.password;
           res.redirect('/navegacion');
-          
+
         } else {
+          
           res.redirect('/');
         }
       } else {
+       
         res.redirect('/');
       }
 
     });
   },
 
-  getMenu: async function(req, res){ //NAVEGACION
+  getMenu: async function (req, res) { //NAVEGACION
     if (req.session.username) {
 
-      res.render('inicio.html');
+      res.render('menu.html');
 
     } else {
       res.redirect('/');
     }
-  }, 
+  },
 
   getUser: async function (req, res) { //LISTA DE USUARIOS EN BD
     if (req.session.username) {
 
       await pool.query('SELECT * FROM login', function (err, resultado, fields) {
         const datos = resultado.rows;
-        res.render('inicio.html', { datos });
+        res.render('users.html', { datos });
       });
 
     } else {
@@ -69,7 +71,7 @@ module.exports = {
       const contrasenia = encrip.hashSync(password, salt);
 
       await pool.query('UPDATE login set contrasenia=$2 where id_usuario=$1', [username, contrasenia], (error, results) => {
-        
+
         if (error) {
           throw error;
         } else {
@@ -85,7 +87,7 @@ module.exports = {
   postRegisUser: async function (req, res) { //Registrar usuarios
 
     if (req.session.username) {
-      
+
       const { username, password } = req.body;
       const salt = encrip.genSaltSync(10);
       const contrasenia = encrip.hashSync(req.body.password, salt);
--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const ruta = express.Router();
 const db = require('../database');
-
+var datos = []; 
 
 ruta.get('/', (req, res) => {  //Login
     if (req.session.username) {
+        datos=[];
         res.redirect('/navegacion');
+        
     } else {
-        res.render('login.html');
+        res.render('login.html', {datos});
+        datos= [true]; 
     }
 });
+ruta.get('/navegacion', db.getMenu); //Navegacion
 
-
-ruta.get('/navegacion', db.getUser); //Navegacion
+ruta.get('/navegacion/users', db.getUser); //Usuarios
 
 ruta.get('/navegacion/newPass/:id', function (req, res) {  // Cambiar  contraseña
     if (req.session.username) {
-
         const username = req.params.id;
         res.render('newPass.html', { username });
     } else {
-        res.render('login.html');
+        res.redirect('/');
     }
 
 
@@ -31,15 +33,17 @@ ruta.get('/navegacion/signIn', function (req, res) { //Registrar nuevo usuario
 
         res.send('aqui va el html para poder registrar')
     } else {
-        res.render('login.html');
+        res.redirect('/');
     }
 });
 
 ruta.get('/navegacion/logout', (req, res) => { //Cerrar sesion 
+
     req.session.destroy(function (err) {
         if (err) {
             console.log(err);
         } else {
+            datos=[]; 
             res.redirect('/');
         }
     });
